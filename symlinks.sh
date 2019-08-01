4a1e819298d087f3ffa5d8afc8384a3b8e56138d
Uladzimir Halushka <Uladzimir_Halushka@epam.com>
2019-08-01 14:26:04 +0300

    Update symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 # WARNING: Please backup your .dotfiles before using the script
 
-# This script move dotfiles (.zshrc and .gitconfig) to the current directory 
+# This script moves dotfiles (.zshrc and .gitconfig) to the current directory 
 # and creates symlinks to them at the root. 
 # The idea is to move all config files to a folder syncing with some cloud.
 mv ~/.gitconfig ./
